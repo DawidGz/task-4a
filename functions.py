0f233ed8d7ceff94013823c6b24006593c4d5a0c
d_zydek18 <d_zydek@cranford.hounslow.sch.uk>
2024-03-07 13:42:12 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+# GBP to USD
+
+def covert_usd_to_gbp(amount_local):
+ """take the USD and convert it into GBP """
+ amount_usd = amount_local * 0.79
+ return amount_usd
+
+
+# USD to GBP
+
+def convert_gbp_usd(amount_local):
+    """take the GBP and convert it into usd """
+
+
+
+
+
+
